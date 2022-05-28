9d8c85332386fd867ef158066cfe591f361cac3c
Fazil Babu <fazil47babu@gmail.com>
2022-05-28 16:04:33 +0530

    Customer cards look okay, but are not aligned

---
+++ src/components/CustomerCard.tsx
@@ -0,0 +1,41 @@
+import "./CustomerCard.css";
+
+export default function CustomerCard({
+  logo,
+  name,
+  designation,
+  description,
+  services,
+  image,
+}: {
+  logo: string;
+  name: string;
+  designation: string;
+  description: string;
+  services: string[];
+  image: string;
+}) {
+  return (
+    <div className="customerCard">
+      <div className="customerCardContent">
+        <div className="customerCardDetails">
+          <img src={logo} alt={name} className="customerLogo" />
+          <div className="customerPersonal">
+            <h3 className="customerName">{name}</h3>
+            <h3 className="customerDesignation">{designation}</h3>
+          </div>
+          <p className="customerDescription">{description}</p>
+        </div>
+        <img src={image} alt={name} className="customerImage" />
+      </div>
+      <div className="customerCardFooter">
+        <div className="customerCardServicesText">Services provided: </div>
+        <div className="customerCardServicesProvided">
+          {services.map((service) => {
+            return <div className="customerCardService">{service}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
